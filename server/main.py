1e86357ff94a1748c9916500d128d56f1e0e9149
Abel Palmer <abelpalmer22@gmail.com>
2026-01-16 01:07:29 -0700

    Finished for Jan 16: made client and server ui's and got them to work

--- server/main.py
+++ server/main.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/analyze")
 async def analyze_audio(file: UploadFile = File(...)):
     _ = await file.read()
     return {
-            "transcript": "hello world",
-            "words": [
-                {"text": "hello", "start_ms": 0, "end_ms": 500},
-                {"text": "world", "start_ms": 500, "end_ms": 1000}
-                ],
-            "feedback": []
-            }
+        "transcript": "hello world",
+        "words": [
+            {"text": "hello", "start_ms": 0, "end_ms": 500},
+            {"text": "world", "start_ms": 500, "end_ms": 1000},
+        ],
+        "feedback": [],
+    }
+
